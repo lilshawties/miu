54ac5bff864b899919e594a775a7152c35c2ef19
bomie <bomidelrey@gmail.com>
2024-10-12 20:17:35 +0700

    config.js

--- config.js
+++ config.js
@@ -21,16 +21,16 @@
 
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["506398176077545482", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
-  embedColor: "#1db954",
-  musicardTheme:"themes16", //Goes from themes1 to themes19
-  activityName: "You", // This is bot status Write Anything here 
-  activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
-  CheckmarkIcon: "https://cdn.discordapp.com/emojis/819446784647757834.gif",
-  MusicIcon:"https://cdn.discordapp.com/emojis/763415718271385610.gif",
+  embedColor: "#bc8787",
+  musicardTheme:"themes5", //Goes from themes1 to themes19
+  activityName: ".𝘣𝘰𝘮𝘪𝜗𝜚˚⋆", // This is bot status Write Anything here 
+  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  SupportServer: "https://discord.gg/PbMBtxkTGw",
+  CheckmarkIcon: "https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif",
+  MusicIcon:"https://media.discordapp.net/attachments/1230824451990622299/1236664581364125787/music-play.gif",
   embedTimeout: 5,  // Timeout before the button interaction embeds are deleted ( Default - 5 seconds)
   errorLog: "", 
 
